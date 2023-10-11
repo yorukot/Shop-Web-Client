b24bbca51a621850acd2ad4ea074748cf11dd49e
Night Cat <64583797+bengod1020@users.noreply.github.com>
2023-10-12 00:21:01 +0800

    update cart

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,4 @@
 const env = process.env.NODE_ENV
 
 export const API_URL = (env == "development" ? 'https://my-computer.nightcat.xyz/api': 'https://my-computer.nightcat.xyz/api')
-export const WEBSITE_URL = 'https://my-computer.nightcat.xyz/'
+export const WEBSITE_URL = 'https://shop-web-api.nightcat.xyz/'
