3f52df6ffaa90cc622d756b5337052bd847ba686
Night Cat <64583797+bengod1020@users.noreply.github.com>
2023-10-12 18:12:58 +0800

    update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {rewrites: async () => {
     return [
       {
         source: "/api/:path*",
-        destination:"https://shop-web-api.nightcat.xyz/api/:path*",
+        destination:"https://shop-web-api.nightcat.xyz/api",
       }]}}
 
 module.exports = nextConfig
