e11df589347dce09d1450c41ba97d62d8b971832
Night Cat <107802416+MHNightCat@users.noreply.github.com>
2023-10-14 17:16:29 +0800

    update config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const env = process.env.NODE_ENV;
 
-const API_URL = (env === "development" ? 'https://api-test.nightcat.xyz/api': 'https://shop-web.nightcat.xyz/api')
+const API_URL = (env === "development" ? 'https://api-test.nightcat.xyz/api': 'https://shop-web-api.nightcat.xyz/api')
 
 const nextConfig = {
   reactStrictMode: false,
