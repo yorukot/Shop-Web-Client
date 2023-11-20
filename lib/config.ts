2c6e71e4b1193108429d4c1d6148a9e0823e8f1f
MHNightCat <107802416+MHNightCat@users.noreply.github.com>
2023-11-20 22:47:26 +0800

    update

--- lib/config.ts
+++
@@ -1,4 +0,0 @@
-const env = process.env.NODE_ENV
-
-export const API_URL = (env === "development" ? 'https://my-computer.nightcat.xyz/api': 'https://pccuhort.com/api')
-export const WEBSITE_URL = (env === "development" ? 'https://my-computer.nightcat.xyz/': 'https://pccuhort.com/')
