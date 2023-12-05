9cd8cc901fb1d3d5b3db748f3c963d23d1db1f40
MHNightCat <107802416+MHNightCat@users.noreply.github.com>
2023-12-05 18:28:16 +0800

    update

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,10 +1,31 @@
-<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-table-down" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M12.5 21h-7.5a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v7.5" />
-  <path d="M3 10h18" />
-  <path d="M10 3v18" />
-  <path d="M19 16v6" />
-  <path d="M22 19l-3 3l-3 -3" />
-</svg>
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import middlewareCheckPressiom from '@/functions/Get/middlewareCheckPressiom';
+import { notFound } from 'next/navigation'
 
+export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/admin')){
+    const url = request.nextUrl;
+    const VerifyPerssiom = await middlewareCheckPressiom(request.headers)
+    if(VerifyPerssiom?.admin !== true){
+        url.pathname = `/404`;
+        return NextResponse.rewrite(url);
+    }
+    const response = NextResponse.next();
+    return response
+  }
+  if (request.nextUrl.pathname.startsWith('/user')){
+    const url = request.nextUrl;
+    const VerifyPerssiom = await middlewareCheckPressiom(request.headers)
+    if(!VerifyPerssiom.sub){
+        url.pathname = `/loginfirst`;
+        return NextResponse.rewrite(url);
+    }           
+    const response = NextResponse.next();
+    return response
+  }
+}
 
+export const config = {
+  matcher: ['/admin/:path*', '/admin', '/user/:path*', '/user'],
+};
